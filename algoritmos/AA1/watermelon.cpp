17afd24477e1208d1d8bfd16068ffb4dd3ccccef
Henrique Souza <henriquerique233@gmail.com>
2026-09-18 14:05:53 +0000

    Adicionar pasta

---
+++ algoritmos/AA1/watermelon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    if (t % 2 == 0 & t =! 2){
+        cout << "YES" << endl;
+    }  else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
